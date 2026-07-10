d89b899aded03928899148c1c19b8975ddc5cb00
NareshR06 <naresh.r06@gmail.com>
2026-07-10 06:30:36 +0530

    commented 40th line

--- tests/apitest1.spec.js
+++ tests/apitest1.spec.js
@@ -37,5 +37,5 @@ const response = await request.post('https://restful-booker.herokuapp.com/bookin
     console.log('Successfully validated the firstname');
     console.log('Successfully pushed code to github');
     console.log('tester2 did changes');
-    console.log('test');
+    //console.log('test');
 });
